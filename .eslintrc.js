8aa1701af00192958c6b5927462d734f979eb270
pagnkelly <632228198@qq.com>
2022-09-09 15:25:44 +0800

    feat: setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,33 @@
-module.exports = {
-  extends: ['@mpxjs'],
+// module.exports = {
+//   extends: ['@mpxjs'],
+//   rules: {
+//     eqeqeq: 2
+//   }
+// }
+const eslintConf = {
+  extends: ['eslint:recommended', 'plugin:mpx/composition-api-essential'],
+  env: {
+    node: true,
+    jest: true
+  },
+  globals: {
+    wx: 'readonly',
+    my: 'readonly',
+    getCurrentPages: 'readonly',
+    getApp: 'readonly',
+    App: 'readonly',
+    __mpx_mode__: 'readonly',
+    __mpx_env__: 'readonly',
+    __env__: 'readonly',
+    __VERSION__: 'readonly',
+    requirePlugin: 'readonly'
+  },
   rules: {
-    eqeqeq: 2
-  }
+    // camelcase: ['error', { 'allow': ['__mpx_mode__', '__mpx_env__'] }],
+    camelcase: 0,
+    'mpx/no-reserved-keys': 0,
+    'no-undef': 0
+  },
 }
+
+module.exports = eslintConf
